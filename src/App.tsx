53af63b17aaa0b448fb935ff3d4fb917a6f77c73
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-08-07 13:54:51 +0100

    implementing header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Header from "./components/Header";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <CssBaseline />
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
