c76bf3ef74fb36ed70a2bbf3b51acbd296ff52c2
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-08-08 23:32:45 +0100

    started implementing login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,5 +1,25 @@
+import { Box, TextField } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import Header from "./Header";
 
 export default function LoginPage() {
-    return <>
-    <h1>I'm working</h1></>
+  return (
+    <>
+      <Header />
+      <Box maxWidth={1000} mx="auto" p={3} mb={3}>
+        <FormControl>
+          <TextField
+            helperText="Please enter your name"
+            id="demo-helper-text-aligned"
+            label="Name"
+          />
+          <TextField
+            helperText="Please enter your name"
+            id="demo-helper-text-aligned"
+            label="Name"
+          />
+        </FormControl>
+      </Box>
+    </>
+  );
 }
