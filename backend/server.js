2e7c001c84b708d7688e89e0cec78a34f5b59155
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-08-10 00:42:05 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
