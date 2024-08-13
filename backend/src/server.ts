5f187a5a65406e0b088b055d1e11d82fd3553c89
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-08-13 14:14:31 +0100

    refactored backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,12 +8,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/health", (req, res) => {
+  res.send("I'm working");
 });
 
 app.get("/articles", (req, res) => {
-  console.log(req.params);
   const dataToSend = blogData.map((element) => {
     const mappedData = {
       img: element.img,
@@ -30,31 +29,34 @@ app.get("/articles", (req, res) => {
 app.get("/articles/:id", (req, res) => {
   const id = req.params.id;
   const idNum = Number(id);
-  if (!isNaN(idNum)) {
-    const elementMatchingData = blogData.find((element) => {
-      return element.id === idNum;
-    });
-    if (elementMatchingData) {
-      const mappedData = {
-        img: elementMatchingData.img,
-        alt: elementMatchingData.alt,
-        title: elementMatchingData.title,
-        content: elementMatchingData.content,
-        author: elementMatchingData.author,
-        createdAt: elementMatchingData.createdAt,
-        id: elementMatchingData.id,
-      };
-      res.json(mappedData);
-    } else {
-      res.status(404).json({
-        errorMessage: `Article with id ${idNum} does not exist`,
-      });
-    }
-  } else {
+  if (isNaN(idNum)) {
     res.status(400).json({
-      errorMessage: `ID parameter must be a number`
-    })
+      errorMessage: `ID parameter must be a number`,
+    });
+    return;
   }
+
+  const elementMatchingData = blogData.find((element) => {
+    return element.id === idNum;
+  });
+
+  if (!elementMatchingData) {
+    res.status(404).json({
+      errorMessage: `Article with id ${idNum} does not exist`,
+    });
+    return;
+  }
+
+  const mappedData = {
+    img: elementMatchingData.img,
+    alt: elementMatchingData.alt,
+    title: elementMatchingData.title,
+    content: elementMatchingData.content,
+    author: elementMatchingData.author,
+    createdAt: elementMatchingData.createdAt,
+    id: elementMatchingData.id,
+  };
+  res.json(mappedData);
 });
 
 app.listen(port, () => {
