473266711a0570161c6f48d5fc7222b816abdb7d
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-08-14 11:22:23 +0100

    started  NewBlogPage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/health", (req, res) => {
+app.get("/", (req, res) => {
   res.send("I'm working");
 });
 
--- frontend/src/components/NewBlogPage.tsx
+++ frontend/src/components/NewBlogPage.tsx
@@ -3,14 +3,47 @@ import { Box, FormControl, TextField } from "@mui/material";
 export default function NewBlogPage() {
   return (
     <>
-      <Box maxWidth={1000} mx="auto" p={3} mb={3}>
-        <FormControl>
-          <TextField
-            helperText="Please enter your name"
-            id="author-name"
-            label="Author name"
-          />
-        </FormControl>
+      <Box
+        display="inline-flex"
+        position="relative"
+        alignItems="stretch"
+        flexDirection="column"
+        component="form"
+        maxWidth={1000}
+        mx="auto"
+        p={3}
+        mb={3}
+      >
+        <TextField
+          margin="normal"
+          helperText="Please enter the title for your blog"
+          id="title"
+          label="Title"
+          variant="outlined"
+        />
+        <TextField
+          margin="normal"
+          helperText="Please enter your name or leave anonomous"
+          id="author"
+          label="Author Name"
+          variant="outlined"
+        />
+        <TextField
+          margin="normal"
+          helperText="Please enter an article summary up to 400 chracters"
+          id="excerpt"
+          label="article summary"
+          minRows={3}
+          multiline
+        />
+        <TextField
+          margin="normal"
+          helperText="Please enter the content of your article"
+          id="content"
+          label="Article Content"
+          minRows={3}
+          multiline
+        />
       </Box>
     </>
   );
